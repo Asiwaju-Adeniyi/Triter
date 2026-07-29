f95f8357883b0338f24333ff1c49cc4c665084e4
Asiwaju-Adeniyi <makindeadeniyi1@gmail.com>
2026-07-29 19:43:35 +0100

    PyTorch softmax

--- Softmax.py
+++ Softmax.py
@@ -1,1 +1,9 @@
+import torch 
+import triton
+import triton.language as tl
+import torch.nn.functional as F
 
+
+union = torch.tensor([[12,34,35,35,36,70], [1,23,46,90,43,35]], dtype=torch.float32, device = 'cuda')
+sam = F.softmax(union, dim = 1)
+print(F"{sam}")
