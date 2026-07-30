6a08a1ca3ecf5a97ea9a8ac07444ec8c8a761003
Asiwaju-Adeniyi <makindeadeniyi1@gmail.com>
2026-07-30 02:18:36 +0100

    Update Softmax.py

--- Softmax.py
+++ Softmax.py
@@ -15,8 +15,39 @@ def naive_softmax(x: torch.Tensor)-> torch.Tensor:
 
   @triton.jit 
 
-  def _softmax_fwd_kernel(): 
-    pass 
+  def _softmax_fwd_kernel(
+      output_ptr,
+      stride_output_row, 
+      input_ptr,
+      stride_input_row,
+      num_cols,
+      block_size: tl.constexpr,
+  ): 
+  #setup input ptrs
+    row_index = tl.program_id(0) 
+    row_start_ptr = input_ptr + (row_index * stride_input_row)
+    col_offsets = tl.arange(0, block_size)
+    input_pointers = row_start_ptr + col_offsets
+
+    row_mask = col_offsets < num_cols
+    #move to SRAM
+
+    row = tl.load(input_pointers,mask = row_mask, other = float("-inf"))
+
+    #softmax itself
+    safe_row = row - row_max  
+    numerator = tl.exp(safe_row)
+    denominator = tl.sum(numerator, axis=0)
+    sm_out = numerator / denominator 
+
+    #write back to HBM 
+
+    output_row_ptr = output_ptr + (row_index * strdie_output_row)
+    output_pointers = output_ptr + col_offsets
+    tl.store(output_pointers, sm_out, mask=row_mask)
+
+
+
 
   def softmax(x:torch.Tensor)->torch.Tensor: 
     """ Triton impl of Softmax, fwd pass only """
