7abca35f0a858cfb03ae4de66f26e59404db49b5
Asiwaju-Adeniyi <makindeadeniyi1@gmail.com>
2026-07-30 02:28:43 +0100

    Update Softmax.py

--- Softmax.py
+++ Softmax.py
@@ -1,9 +1,9 @@
-import torch 
+import torch
 import triton
 import triton.language as tl
 import torch.nn.functional as F
 
-def naive_softmax(x: torch.Tensor)-> torch.Tensor: 
+def naive_softmax(x: torch.Tensor)-> torch.Tensor:
   """ eager mode softmax """
 
   x_max = x.max(dim=1)[0]
@@ -13,18 +13,18 @@ def naive_softmax(x: torch.Tensor)-> torch.Tensor:
   sm_out = numerator/denominator[:, None]
   return sm_out
 
-  @triton.jit 
+  @triton.jit
 
   def _softmax_fwd_kernel(
       output_ptr,
-      stride_output_row, 
+      stride_output_row,
       input_ptr,
       stride_input_row,
       num_cols,
       block_size: tl.constexpr,
-  ): 
+  ):
   #setup input ptrs
-    row_index = tl.program_id(0) 
+    row_index = tl.program_id(0)
     row_start_ptr = input_ptr + (row_index * stride_input_row)
     col_offsets = tl.arange(0, block_size)
     input_pointers = row_start_ptr + col_offsets
@@ -35,12 +35,12 @@ def naive_softmax(x: torch.Tensor)-> torch.Tensor:
     row = tl.load(input_pointers,mask = row_mask, other = float("-inf"))
 
     #softmax itself
-    safe_row = row - row_max  
+    safe_row = row - row_max
     numerator = tl.exp(safe_row)
     denominator = tl.sum(numerator, axis=0)
-    sm_out = numerator / denominator 
+    sm_out = numerator / denominator
 
-    #write back to HBM 
+    #write back to HBM
 
     output_row_ptr = output_ptr + (row_index * strdie_output_row)
     output_pointers = output_ptr + col_offsets
@@ -49,10 +49,10 @@ def naive_softmax(x: torch.Tensor)-> torch.Tensor:
 
 
 
-  def softmax(x:torch.Tensor)->torch.Tensor: 
+  def softmax(x:torch.Tensor)->torch.Tensor:
     """ Triton impl of Softmax, fwd pass only """
 
-     rows, cols = x.shape 
+     rows, cols = x.shape
      assert x.dim() ==2, f"only accepts 2D tensors for now"
      block_size = triton.next_power_of_2(cols)
      num_warps = 4  # *32
@@ -60,19 +60,19 @@ def naive_softmax(x: torch.Tensor)-> torch.Tensor:
           num_warps = 8
     if block_size > 4095: #4096
           num_warps = 16
-    
+
     grid = (rows, )
 
-    #allocate out output buffer 
+    #allocate out output buffer
     sm_out = torch.empty_like(x)
 
 
      _softmax_fwd_kernel[grid] (
-         sm_out, 
-         sm_out.stride(0), 
-         x, 
-         x.stride(0), 
-         cols, 
+         sm_out,
+         sm_out.stride(0),
+         x,
+         x.stride(0),
+         cols,
          block_size=block_size,
          num_warps, =num_warps
 
@@ -88,3 +88,6 @@ print(F"{sam}")
 
 eager_out = naive_softmax(sam)
 print(f"{eager_out=}")
+
+triton_out = softmax(union)
+printf(f"{triton_out=}")
